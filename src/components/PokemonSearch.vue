f58f64dd22fe2b146d10f7be59626cfdb1934000
Cl84Dev <cl84dev@yandex.com>
2023-02-13 18:19:59 -0300

    Funcionalidade de Evolution Chain implementada depois do horário indicado. Por isso não fiz o deploy.

--- src/components/PokemonSearch.vue
+++ src/components/PokemonSearch.vue
@@ -33,46 +33,16 @@ export default {
   data() {
     return {
       input_search: "",
-      // poke_name: "",
-      // poke_img: "",
-      // hp: "",
-      // attack: "",
-      // defense: "",
-      // special_attack: "",
-      // special_defense: "",
-      // speed: "",
       show_error: false,
-      show_stats: false,
       poke_data_chain: null,
     };
   },
   methods: {
-    // async sendName() {
-    //   try {
-    //     const response = await axios.get(
-    //       `https://pokeapi.co/api/v2/pokemon/${this.input_search.toLowerCase()}`
-    //     );
-    //     this.poke_name = response.data.name;
-    //     this.poke_img = response.data.sprites.other.dream_world.front_default;
-    //     this.hp = response.data.stats[0].base_stat;
-    //     this.attack = response.data.stats[1].base_stat;
-    //     this.defense = response.data.stats[2].base_stat;
-    //     this.special_attack = response.data.stats[3].base_stat;
-    //     this.special_defense = response.data.stats[4].base_stat;
-    //     this.speed = response.data.stats[4].base_stat;
-    //     this.show_error = false;
-    //     this.show_stats = false;
-    //   } catch (error) {
-    //     console.error(error);
-    //     this.poke_name = null;
-    //     this.show_error = true;
-    //   }
-    // },
-    showStats() {
-      this.show_stats = !this.show_stats;
-    },
-    async evolutionChain() {
+    async searchPokemon() {
       try {
+        this.poke_data_chain = [];
+        this.show_error = false;
+
         const response = await axios.get(
           `https://pokeapi.co/api/v2/pokemon/${this.input_search.toLowerCase()}`
         );
@@ -80,46 +50,53 @@ export default {
         const evolutionChain = await axios.get(
           species.data.evolution_chain.url
         );
-        const first = evolutionChain.data.chain.species.name;
 
-        this.poke_data_chain = [];
-        this.show_error = false;
-        this.show_stats = false;
+        const baby = evolutionChain.data.chain.species.name;
 
-        if (await first) {
+        if (baby) {
           const response = await axios.get(
-            `https://pokeapi.co/api/v2/pokemon/${first}`
+            `https://pokeapi.co/api/v2/pokemon/${baby}`
           );
-          this.poke_data_chain.push(response);
+          this.poke_data_chain.push({ show: false, data: response.data });
         }
 
-        const second = first
-          ? evolutionChain.data.chain.evolves_to[0].species.name
-          : null;
+        const evolution = evolutionChain.data.chain.evolves_to;
 
-        if (await second) {
-          const response = await axios.get(
-            `https://pokeapi.co/api/v2/pokemon/${second}`
-          );
-          this.poke_data_chain.push(response);
+        if (evolution) {
+          evolution.map(async (item) => {
+            const response = await axios.get(
+              `https://pokeapi.co/api/v2/pokemon/${item.species.name}`
+            );
+            this.poke_data_chain.push({ show: false, data: response.data });
+          });
         }
 
-        const third = second
-          ? evolutionChain.data.chain.evolves_to[0].evolves_to[0].species.name
-          : null;
+        const secondEvolution = evolution.map(
+          (item, index) => item.evolves_to[index]
+        );
 
-        if (await third) {
-          const response = await axios.get(
-            `https://pokeapi.co/api/v2/pokemon/${third}`
-          );
-          this.poke_data_chain.push(response);
+        if (secondEvolution) {
+          secondEvolution.map(async (item) => {
+            if (item) {
+              const response = await axios.get(
+                `https://pokeapi.co/api/v2/pokemon/${item.species.name}`
+              );
+              this.poke_data_chain.push({ show: false, data: response.data });
+            }
+          });
         }
-
-        console.log(first, second, third, this.poke_data_chain);
       } catch (error) {
+        console.error(error);
         this.poke_data_chain = null;
         this.show_error = true;
       }
+    },
+    showStats(id) {
+      this.poke_data_chain.forEach((item) => {
+        if (item.data.id === id) {
+          item.show = !item.show;
+        }
+      });
     },
   },
 };
@@ -136,62 +113,14 @@ export default {
       aria-label="Search"
       aria-describedby="button-addon2"
     >
-      <MDBBtn color="primary" v-on:click="evolutionChain">
+      <MDBBtn color="primary" v-on:click="searchPokemon">
         <MDBIcon icon="search" />
       </MDBBtn>
     </MDBInput>
-    <!-- <div v-if="show_error && !poke_name">Pokemon not found</div> -->
-    <!-- <div v-if="!show_error && poke_name" class="card p-3">
-      <MDBCard>
-        <a v-mdb-ripple="{ color: 'light' }">
-          <MDBCardImg :src="poke_img" top alt="..." class="img" />
-        </a>
-        <MDBCardBody class="d-flex flex-column align-items-center">
-          <MDBCardTitle>{{ poke_name.toUpperCase() }}</MDBCardTitle>
-          <MDBBtn
-            tag="a"
-            href="#!"
-            color="primary"
-            class="my-3"
-            v-on:click="showStats"
-            >{{ !show_stats ? "Show Stats" : "Hide Stats" }}</MDBBtn
-          >
-        </MDBCardBody>
-      </MDBCard>
-      <MDBCard
-        style="width: 100%"
-        class="align-self-center my-3"
-        v-if="show_stats"
-      >
-        <MDBListGroup flush>
-          <MDBListGroupItem
-            ><span class="fw-bold">HP: </span>{{ hp }}</MDBListGroupItem
-          >
-          <MDBListGroupItem
-            ><span class="fw-bold">Attack: </span>{{ attack }}</MDBListGroupItem
-          >
-          <MDBListGroupItem
-            ><span class="fw-bold">Defense: </span
-            >{{ defense }}</MDBListGroupItem
-          >
-          <MDBListGroupItem
-            ><span class="fw-bold">Special Attack: </span
-            >{{ special_attack }}</MDBListGroupItem
-          >
-          <MDBListGroupItem
-            ><span class="fw-bold">Special Defense: </span
-            >{{ special_defense }}</MDBListGroupItem
-          >
-          <MDBListGroupItem
-            ><span class="fw-bold">Speed: </span>{{ speed }}</MDBListGroupItem
-          >
-        </MDBListGroup>
-      </MDBCard>
-    </div> -->
     <div v-if="show_error && !poke_data_chain">Pokemon not found</div>
     <div
       v-if="!show_error && poke_data_chain"
-      class="card p-3"
+      class="card p-3 my-3 border"
       v-for="poke_data in poke_data_chain"
       :key="poke_data.data"
     >
@@ -200,8 +129,7 @@ export default {
           <MDBCardImg
             :src="poke_data.data.sprites.other.dream_world.front_default"
             top
-            alt="..."
-            class="img"
+            :alt="`${poke_data.data.name.toUpperCase()}`"
           />
         </a>
         <MDBCardBody class="d-flex flex-column align-items-center">
@@ -211,15 +139,15 @@ export default {
             href="#!"
             color="primary"
             class="my-3"
-            v-on:click="showStats"
-            >{{ !show_stats ? "Show Stats" : "Hide Stats" }}</MDBBtn
+            v-on:click="showStats(poke_data.data.id)"
+            >{{ !poke_data.show ? "Show Stats" : "Hide Stats" }}</MDBBtn
           >
         </MDBCardBody>
       </MDBCard>
       <MDBCard
         style="width: 100%"
         class="align-self-center my-3"
-        v-if="show_stats"
+        v-if="poke_data.show"
       >
         <MDBListGroup flush>
           <MDBListGroupItem
